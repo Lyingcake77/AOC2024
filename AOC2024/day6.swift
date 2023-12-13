951edf687e42d024ac2c1d78153a0c096addc5ef
Dan <lyingcake77@Gmail.com>
2023-12-13 00:23:38 -0500

    day6a setup complete

--- AOC2024/day6.swift
+++ AOC2024/day6.swift
@@ -6,17 +6,26 @@
 //
 
 import Foundation
-class newClass2{
-    
+
+struct raceStat{
+    var time:Int
+    var distance:Int
 }
 
-func convertSomething2(raceRecordsRaw: [String]) -> [newClass]{
-    var newClasses: [newClass]=[]
+func convertSomething2(raceRecordsRaw: [String]) -> [raceStat]{
     
-    return newClasses
+    let times = raceRecordsRaw[0].split(separator: " ")
+    let distances = raceRecordsRaw[1].split(separator: " ")
+    let newRaceStats: [raceStat]=[
+        raceStat(time: Int(times[1])!, distance: Int(distances[1])!),
+        raceStat(time: Int(times[2])!, distance: Int(distances[2])!),
+        raceStat(time: Int(times[3])!, distance: Int(distances[3])!),
+        raceStat(time: Int(times[4])!, distance: Int(distances[4])!),
+    ]
+    return newRaceStats
 }
 func day6a_func(raceRecordsRaw: [String]) throws -> Int{
-    var newClasses: [newClass]=convertSomething2(raceRecordsRaw: raceRecordsRaw)
+    var newRaceStats: [raceStat]=convertSomething2(raceRecordsRaw: raceRecordsRaw)
     var total = 0
   
                   
@@ -25,7 +34,7 @@ func day6a_func(raceRecordsRaw: [String]) throws -> Int{
 }
 
 func day6b_func(raceRecordsRaw: [String]) throws -> Int{
-    var newClasses: [newClass]=convertSomething2(raceRecordsRaw: raceRecordsRaw)
+    var newRaceStats: [raceStat]=convertSomething2(raceRecordsRaw: raceRecordsRaw)
     var total = 0
   
     
