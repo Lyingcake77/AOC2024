7457e8807361d033d67a8acef71763049a4d1dfe
Dan <lyingcake77@Gmail.com>
2023-12-12 20:11:02 -0500

    day 4b complete (untested)

--- AOC2024/day4.swift
+++ AOC2024/day4.swift
@@ -13,6 +13,7 @@ class scratchCard{
     //Number; Order
     var givenNumbers: [Int]
     var winningCount: Int = 0
+    var copies: Int = 1
     
     init(cardNumber: Int, winningNumbers: [Int], givenNumbers: [Int], winningCount:Int) {
         self.cardNumber = cardNumber
@@ -64,9 +65,43 @@ func day4a_func(scratchCardsRaw: [String]) throws -> Int{
     
     return total
 }
+//TODO: see if we can do an extention on the original method and apply this effect
 func day4b_func(scratchCardsRaw: [String]) throws -> Int{
-  
+    var scratchCards: [scratchCard]=convertScratchCards(scratchCardsRaw: scratchCardsRaw)
     var total = 0
-   
+    //convoluded rules:
+    //for first scartch card; there will always be 1 copy ever.
+    //If a card has winning numbers, increase the copy count of the next X amount of cards
+        //this effect applies for every copy of the card
+    //no effect can hapen after the end of the list.
+    
+    let scratchCardsCount = scratchCards.count
+    var i=0
+    while i < scratchCardsCount{
+        let winner = scratchCards[i].winningCount
+        let copies = scratchCards[i].copies
+        
+        //once we get here, its safe that no more copies will be made so we add it to the count
+        total += copies
+        
+        var currentCopy = 1
+        //loop through ever time this effect is applied
+        while currentCopy <= copies{
+            
+            //now we add 1 for the next {X} cards based on winning numbers
+            var j = 1
+            while j <= winner{
+                //check to make sure we arent going past the available records
+                if i+j < scratchCardsCount{
+                    //increment the amount of copies
+                    scratchCards[i+j].copies = scratchCards[i+j].copies + 1
+                }
+                j+=1
+            }
+            currentCopy+=1
+        }
+        i+=1
+    }
+
     return total
 }
