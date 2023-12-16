1049784df8b3f9b487dae5981e32468a645bf6ff
Dan <lyingcake77@Gmail.com>
2023-12-15 22:05:57 -0500

    day 7a complete
    7b.... we'll do the mathy way

--- AOC2024/day6.swift
+++ AOC2024/day6.swift
@@ -25,10 +25,34 @@ func convertSomething2(raceRecordsRaw: [String]) -> [raceStat]{
     return newRaceStats
 }
 func day6a_func(raceRecordsRaw: [String]) throws -> Int{
-    var newRaceStats: [raceStat]=convertSomething2(raceRecordsRaw: raceRecordsRaw)
-    var total = 0
-  
-                  
+    let newRaceStats: [raceStat] = convertSomething2(raceRecordsRaw: raceRecordsRaw)
+    
+    var total = 1
+    //theres a math answer here
+    for race in newRaceStats{
+        var possibleOptions = 0
+        
+        var raceDistance = race.distance
+        var raceTime = race.time
+        var raceTimeHalfWayPoihnt = raceTime/2
+        
+        for timeHeld in 0...raceTime{
+            var maxDistance = timeHeld * (raceTime - timeHeld)
+            if (maxDistance > raceDistance){
+                possibleOptions+=1
+//                if raceTime > raceTimeHalfWayPoihnt{
+//                    possibleOptions = raceTime - timeHeld
+//                }
+//                else{
+//                    possibleOptions = (raceTime - timeHeld) * 2
+//                    
+//                }
+//                break
+            }
+        }
+        print(possibleOptions)
+        total *= possibleOptions
+    }
     
     return total
 }
@@ -37,6 +61,35 @@ func day6b_func(raceRecordsRaw: [String]) throws -> Int{
     var newRaceStats: [raceStat]=convertSomething2(raceRecordsRaw: raceRecordsRaw)
     var total = 0
   
+    let newRaceStats: [raceStat] = convertSomething2(raceRecordsRaw: raceRecordsRaw)
+    
+    var total = 1
+    //theres a math answer here
+    var possibleOptions = 0
+    
+    var raceDistance = Int(String(newRaceStats[0].distance)+String(newRaceStats[1].distance)+String(newRaceStats[2].distance)+String(newRaceStats[3].distance))
+    var raceTime = Int(String(newRaceStats[0].time)+String(newRaceStats[1].time)+String(newRaceStats[2].time)+String(newRaceStats[3].time))
+    var raceTimeHalfWayPoihnt = raceTime/2
+    
+    for timeHeld in 0...raceTime{
+        var maxDistance = timeHeld * (raceTime - timeHeld)
+        if (maxDistance > raceDistance){
+            possibleOptions+=1
+//                if raceTime > raceTimeHalfWayPoihnt{
+//                    possibleOptions = raceTime - timeHeld
+//                }
+//                else{
+//                    possibleOptions = (raceTime - timeHeld) * 2
+//
+//                }
+//                break
+        }
+    }
+    print(possibleOptions)
+    total *= possibleOptions
+    
     
     return total
+    
+    
 }
